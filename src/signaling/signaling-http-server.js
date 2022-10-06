e91efd3b2c845fcde440ef53b0bccf081d628f2f
wangrongding <wangrongding@qq.com>
2022-10-07 01:58:01 +0800

    chore: 添加 nginx 配置文件备份,修改请求头

--- src/signaling/signaling-http-server.js
+++ src/signaling/signaling-http-server.js
@@ -8,7 +8,20 @@ const port = 3000;
 const app = express();
 
 // 解决了所有请求头和方式设置的繁琐问题,要携带cookie时，这种方式不适合
-app.use(cors());
+// app.use(cors());
+// =======
+//设置跨域访问
+app.all("*", (req, res, next) => {
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin", "*");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers", "content-type");
+  //跨域允许的请求方式
+  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+  //让options尝试请求快速结束
+  if (req.method.toLowerCase() == "options") res.send(200);
+  else next();
+});
 
 // 随便写一个接口测试一下
 app.get('/', (req, res) => {
